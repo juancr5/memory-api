5cca8e7173c51ee28ae538ae6395d508c5c5cbdb
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2019-08-14 13:48:50 -0500

    punto 4 corregido

--- Respuestas/malloc.c
+++ Respuestas/malloc.c
@@ -1,7 +1,23 @@
-#include <stdio.h>
 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
 {
-    char *p = malloc(1e50);
+    int memory = atoi(argv[1]) * 1E6;
+    int time = atoi(argv[2]);
+    printf("Memoria separada = %p Megas\n", argv[1]);
+    char *p = malloc(memory);
+
+    clock_t time_t;
+    time_t = clock();
+    while ((clock() - time_t) / CLOCKS_PER_SEC < time)
+    {
+        for (int i = 0; i < memory; i++)
+        {
+            p[i] = 0;
+        }
+    }
     return 0;
 }
